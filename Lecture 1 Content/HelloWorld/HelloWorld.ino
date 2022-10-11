20088ab4316f09fadfdd32383bbccf102da79bbf
YI PING <pyzyf1983@gmail.com>
2022-10-11 10:24:02 +0800

    time delay changed
    500ms to 1000ms

--- Lecture 1 Content/HelloWorld/HelloWorld.ino
+++ Lecture 1 Content/HelloWorld/HelloWorld.ino
@@ -4,7 +4,7 @@ void setup() {
 
 void loop() {
   digitalWrite(13, HIGH);    // turn the LED on (HIGH outputs 5V)
-  delay(500);                // wait for 500 milliseconds
+  delay(1000);                // wait for 500 milliseconds
   digitalWrite(13, LOW);     // turn the LED off (LOW outputs 0V)
   delay(500);                // wait for 500 milliseconds
 }
